9dd2726540bd0281b07b700032052a48ee240515
FrancisKOMPAORE <christerence@outlook.com>
2025-01-21 12:43:26 +0000

    Commit

--- js/main.js
+++ js/main.js
@@ -19,9 +19,32 @@ openConsentement.addEventListener("click", (event) =>{
     document.body.classList.remove("close-consentement");
 })
 
+openConsentement.click();
+
 const refuserConsentement = document.querySelector("#consentement-preference");
 
 refuserConsentement.addEventListener("click", () =>{
     event.preventDefault();
     closeConsentement.click();
 })
+
+const closeConfidentialityPolicy = document.querySelector("#close-confidentiality-policy-icon");
+closeConfidentialityPolicy.addEventListener("click", () =>{
+    document.body.classList.add("close-confidentiality-policy-action")
+    document.body.classList.remove("open-confidentiality-policy-action");
+})
+
+const openConfidentialityPolicy = document.querySelector("#politique-de-confidentialite-link");
+openConfidentialityPolicy.addEventListener("click", () =>{
+    event.preventDefault();
+    document.body.classList.remove("close-confidentiality-policy-action");
+    document.body.classList.add("open-confidentiality-policy-action")
+})
+
+const consultConfidentialityPolicy = document.querySelector("#consult-confidentiality-policy");
+consultConfidentialityPolicy.addEventListener("click", () =>{
+    event.preventDefault();
+    openConfidentialityPolicy.click();
+})
+
+closeConfidentialityPolicy.click();
